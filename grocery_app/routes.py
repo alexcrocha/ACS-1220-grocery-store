1f2a5eaa7b8b5ab73c474a662d1a292264e01bb3
Alex Rocha <rocha.amc@gmail.com>
2023-02-05 16:21:57 -0800

    Complete Grocery Store Create Form

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -12,25 +12,38 @@ main = Blueprint("main", __name__)
 #           Routes                       #
 ##########################################
 
-@main.route('/')
+
+@main.route("/")
 def homepage():
     all_stores = GroceryStore.query.all()
     print(all_stores)
-    return render_template('home.html', all_stores=all_stores)
+    return render_template("home.html", all_stores=all_stores)
 
-@main.route('/new_store', methods=['GET', 'POST'])
+
+@main.route("/new_store", methods=["GET", "POST"])
 def new_store():
-    # TODO: Create a GroceryStoreForm
+    # Create a GroceryStoreForm
+    form = GroceryStoreForm()
 
-    # TODO: If form was submitted and was valid:
+    # If form was submitted and was valid:
     # - create a new GroceryStore object and save it to the database,
     # - flash a success message, and
     # - redirect the user to the store detail page.
+    if form.validate_on_submit():
+        new_grocery_store = GroceryStore(
+            title=form.title.data, address=form.address.data
+        )
+        db.session.add(new_grocery_store)
+        db.session.commit()
 
-    # TODO: Send the form to the template and use it to render the form fields
-    return render_template('new_store.html')
+        flash('New store added successfully!')
+        return redirect(url_for('main.store_detail', store_id=new_grocery_store.id))
 
-@main.route('/new_item', methods=['GET', 'POST'])
+    # Send the form to the template and use it to render the form fields
+    return render_template("new_store.html", form=form)
+
+
+@main.route("/new_item", methods=["GET", "POST"])
 def new_item():
     # TODO: Create a GroceryItemForm
 
@@ -40,9 +53,10 @@ def new_item():
     # - redirect the user to the item detail page.
 
     # TODO: Send the form to the template and use it to render the form fields
-    return render_template('new_item.html')
+    return render_template("new_item.html")
 
-@main.route('/store/<store_id>', methods=['GET', 'POST'])
+
+@main.route("/store/<store_id>", methods=["GET", "POST"])
 def store_detail(store_id):
     store = GroceryStore.query.get(store_id)
     # TODO: Create a GroceryStoreForm and pass in `obj=store`
@@ -54,9 +68,10 @@ def store_detail(store_id):
 
     # TODO: Send the form to the template and use it to render the form fields
     store = GroceryStore.query.get(store_id)
-    return render_template('store_detail.html', store=store)
+    return render_template("store_detail.html", store=store)
 
-@main.route('/item/<item_id>', methods=['GET', 'POST'])
+
+@main.route("/item/<item_id>", methods=["GET", "POST"])
 def item_detail(item_id):
     item = GroceryItem.query.get(item_id)
     # TODO: Create a GroceryItemForm and pass in `obj=item`
@@ -68,5 +83,4 @@ def item_detail(item_id):
 
     # TODO: Send the form to the template and use it to render the form fields
     item = GroceryItem.query.get(item_id)
-    return render_template('item_detail.html', item=item)
-
+    return render_template("item_detail.html", item=item)
